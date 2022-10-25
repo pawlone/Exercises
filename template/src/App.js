ac183d49a6e0a8ea079bcac4bfe591267659c19d
Paolo Peroni <paoloper997@gmail.com>
2022-10-25 11:28:18 +0200

    adding exampleApp folder

--- template/src/App.js
+++ template/src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
 import CustomForm from './components/forms/CustomForm'
+import NameSection from './components/classes/NameSection';
+import Counter from './components/stateful/Counter';
 
 function App() {
   return (
     <div className="App">
       <CustomForm></CustomForm>
+      <NameSection name="Pippo Pluto"></NameSection>
+      <Counter></Counter>
     </div>
   );
 }
