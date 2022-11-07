a8d808e46cfff0ad5688da7b52272300b86ea9b2
Paolo Peroni <paoloper997@gmail.com>
2022-11-07 09:03:43 +0100

    finished usermanager exercise

--- usermanager/src/components/UserList/UserList.js
+++ usermanager/src/components/UserList/UserList.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from "react-bootstrap/ButtonGroup"
 import SingleUser from '../SingleUser/SingleUser';
 import { useRest } from '../Hooks/Hooks';
 import { useState } from 'react';
@@ -9,10 +10,10 @@ import { useState } from 'react';
 const urls = "https://reqres.in/api/users?page="
 
 const UserList = () => {
-    const [url, setUrl] = useState(urls + 1)
+    const [url, setUrl] = useState(urls + 1);
 
-    function handleClick() {
-        setUrl(urls + 2)
+    const handleClick = event => {
+        setUrl(urls + event.currentTarget.id)
     }
 
     const { resultData: user, isLoading, error } = useRest(url);
@@ -20,19 +21,22 @@ const UserList = () => {
     if (error) {
         return <div>Error: {error.message}</div>
     }
-    if(isLoading) {
+    if (isLoading) {
         return <div>is loading...</div>
     } else {
         return (
             <Container>
                 <Row className='justify-content-center' xs={1} md={4} >
-                {user.map(p => {
-                    return <SingleUser key={p.id} data = {p} />
-                })}
+                    {user.map(p => {
+                        return <SingleUser key={p.id} data={p} />
+                    })}
                 </Row>
-                <Row className='justify-content-center'>
+                <Row className='justify-content-center my-5'>
                     <Col className='text-center'>
-                    <Button onClick={handleClick}>Page 2</Button>
+                        <ButtonGroup>
+                            <Button id="1" onClick={handleClick} variant="primary">1</Button>
+                            <Button id="2" onClick={handleClick} variant="primary">2</Button>
+                        </ButtonGroup>
                     </Col>
                 </Row>
             </Container>
